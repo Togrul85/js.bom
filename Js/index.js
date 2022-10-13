22914e567e938cfb38ef3e54b19e2f2574a3bb1e
unknown <toghrul@code.edu.az>
2022-10-13 18:51:27 +0400

    .

---
+++ Js/index.js
@@ -0,0 +1,28 @@
+// function onClick($this) {
+//     var val = $this.previousElementSibling.value;
+//     if(val == ''){
+//         document.getElementById("demo").innerHTML ="wrong  input"
+//     }else{
+//         document.getElementById("demo").innerHTML =val;
+//     }
+//   }
+
+const firstname = document.querySelector("#keywordSearch1").value
+const lastname = document.querySelector("#keywordSearch2").value
+const email = document.querySelector("#keywordSearch3").value
+
+const namebox =document.querySelector("#demo1")
+const surnameBox =document.querySelector("#demo2")
+const emailBox = document.querySelector("#demo3")
+
+btn.addEventListener("click", () => {
+      newDiv.classList.add("");
+      newDiv.classList.remove("parag");
+
+
+
+
+
+
+
+
